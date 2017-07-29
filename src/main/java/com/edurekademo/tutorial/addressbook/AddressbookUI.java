4409c498676d5149e4ef1d5c272274c9a6b4c00d
zayaankhan <zayaan.khan@mail.com>
2017-07-29 22:31:35 +0530

    Update AddressbookUI.java
    Changing one line to show in the front end

--- src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
@@ -71,7 +71,7 @@ public class AddressbookUI extends UI {
          */
         newContact.addClickListener(e -> contactForm.edit(new Contact()));
 
-        filter.setInputPrompt("Filter contacts...");
+        filter.setInputPrompt("This is something which i am changing in github");
         filter.addTextChangeListener(e -> refreshContacts(e.getText()));
 
         contactList
